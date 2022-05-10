19f53172da699f54dcd579afd44ba1260c02734a
MSI\裕之 <k021g1294@g.neec.ac.jp>
2022-05-10 12:04:48 +0900

    コミット

---
+++ Assets/Goal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ÉSÅ[ÉãÅI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
